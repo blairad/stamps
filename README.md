a20ddabc9d2b2bb1b9e7fe0071fa5416cd06cfab
Andrew Blair <blair.d.andrew@gmail.com>
2020-10-18 18:03:51 +0100

    wee readme update

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ npm run build --report
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 # stamps
+
+```
+
+To begin this should be a site to display and eventually sell AndoArt stamp gifts and merchandise
+
+```
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="navbar">
+        <div class="nav-content">
+            <router-link to="">
+                <span class="title">Stamps</span>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
